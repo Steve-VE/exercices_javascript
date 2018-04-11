98df2862e635c1d77cc5bdc6ca0c768dbf51c8a3
Steve-VE <steve.vanessche@gmail.com>
2018-04-11 16:36:39 +0200

    Exercice du Pendu et du jeu du Plus ou du Moins

---
+++ index.php
@@ -0,0 +1,29 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/hangman.js"></script>
+    <script src="js/more_or_less.js"></script>
+</head>
+<body>
+    <div class="button" onclick="hangman();">
+        <p>
+            Lancer une nouvelle partie de <strong>Pendu</strong><br>
+        </p>
+        <p class="reminder">    
+            (N'oubliez pas d'ouvrir la console avant !)
+        </p>
+    </div>
+    <div class="button" onclick="moreOrLess();">
+        <p>
+            Jouer au jeu du <strong>Plus ou Moins</strong><br>
+        </p>
+    </div>
+</body>
+</html>
---
+++ js/more_or_less.js
@@ -0,0 +1,72 @@
+
+let moreOrLess = () => { // Fonction qui fait tourner le jeu du plus ou du moins
+    
+    // Minimum
+    let min = 20;
+    // Maximum
+    let max = 80;
+
+    // Le nombre à deviner
+    let secretNumber = random(min, max);
+
+    // La variable qui va stocker la proposition du joueur
+    let myNumber = -1;
+
+    //Le nombre d'essais
+    let count = 0;
+
+    // Variable qui va contenir le message à afficher
+    let message;
+
+    alert("Bienvenue étranger !\n\nTa tâche consistera à trouver un nombre compris entre " + min + " et " + max + " en faisant un minimum de proposition.\n\nBonne chance !");
+
+    while( myNumber != secretNumber ){
+        rawPrompt = prompt("Quelle est ta proposition ?");
+        myNumber = parseInt(rawPrompt);
+
+        count++;
+
+        message = "";
+
+        if(myNumber > secretNumber){
+            message += "Ce n'est pas " + myNumber + ".\n";
+            message += "Le nombre que nous cherchons est plus petit !";
+        }
+        else if(myNumber < secretNumber){
+            message += "Ce n'est pas " + myNumber + ".\n";
+            message += "Le nombre que nous cherchons est plus grand !";
+        }
+        else if(myNumber == secretNumber){
+            message = "Bien joué ! Il s'agit bien du nombre " + secretNumber;
+
+            if(count == 1 ){
+                message += "\nWhoa, incroyable ! Tu as réussi du premier coup !";
+            }
+            else if(count < 6){
+                message += "\nTu as trouvé en " + count + " coups, c'est plutôt bien joué !";
+            }
+            else if(count < 15){
+                message += "\nTu as trouvé en " + count + " coups.";
+            }
+            else if(count < 30){
+                message += "\nTu as trouvé  en " + count + " coups, je suis sûr que tu peux mieux faire.";
+            }
+            else if(count < (max - min)){
+                message += "\nTu as trouvé  en " + count + " coups, c'est franchement pas terrible fieu :/";
+            }
+            else{
+                message += "\nTu as trouvé  en... " + count + " coups !? Tu l'as fais exprès, n'est-ce pas ?";
+            }
+        }
+        else{
+            message = "Que dis-tu !? \"" + rawPrompt + "\" n'est pas un nombre !";
+        }
+
+        alert(message);
+    }
+
+};
+
+let random = (min, max) => { // Fonction qui renvoit un nombre aléatoire compris entre un minimum et un maximum
+    return Math.floor(Math.random() * (max - min)) + min;
+}
